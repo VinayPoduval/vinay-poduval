c05772c43f6b8000dd93852b7dc757192a9f31b2
Vinay Poduval <vinaypoduval@hotmail.com>
2021-01-08 11:14:02 +0530

    Added some css for mobile device

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './Resume.css';
 
 function Resume() {
+    const downloadFile = () => {
+        window.location.href = 'https://githu';
+    }
     return(
         <>
-        <h1>Download My Resume here</h1>
+        <h1>My Resume</h1>
         <div className="resume-container">
-            <p className="p-resume">Click on the button below to download</p>
-            <button className="resume-btn">Download</button>
+            <p className="p-resume">Click on the button below to download my resume</p>
+            <button className="resume-btn" onClick={downloadFile}>Download</button>
         </div>
         </>
     );
